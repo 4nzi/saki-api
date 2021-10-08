05ce3b24e5fa5eb65f84f368577689e29322b679
4nzi <zm17328@josai.ac.jp>
2021-10-08 19:24:17 +0900

    add main functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from 'firebase-functions'
 import * as express from 'express'
+import router from './router'
 
 const app = express()
+app.use('/v1', router)
 
 export const api = functions.https.onRequest(app)
